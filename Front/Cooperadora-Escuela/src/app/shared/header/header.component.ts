2a2d0288ab4a42a23d444d93c9952b9db0641173
Margarita-Bravo <embravo3329@gmail.com>
2025-05-03 17:16:17 -0300

    add footer and header

---
+++ Front/Cooperadora-Escuela/src/app/shared/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+
+  constructor(private router: Router) { }
+
+  logout(): void {
+    // Aquí puedes agregar la lógica para cerrar la sesión (por ejemplo, eliminar el token de autenticación).
+    this.router.navigate(['/login']); // Redirige al login
+  }
+
+}
