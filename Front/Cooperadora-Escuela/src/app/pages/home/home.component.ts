76a1ff61c3bdc933d0d4e79815f26576dd5cde8a
Margarita-Bravo <embravo3329@gmail.com>
2025-05-04 13:14:01 -0300

    agregando home

--- Front/Cooperadora-Escuela/src/app/pages/home/home.component.ts
+++ Front/Cooperadora-Escuela/src/app/pages/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { FooterComponent } from '../../shared/footer/footer.component';
-import { HeaderComponent } from '../../shared/header/header.component';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule,FooterComponent,HeaderComponent],
+  imports: [RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
