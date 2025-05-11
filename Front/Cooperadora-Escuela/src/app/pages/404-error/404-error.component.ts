b3ae8fb9559ce8cc95dc411f2e29cd32f2b95017
yoniea <yea52735@gmail.com>
2025-05-11 17:52:27 -0300

    componente 404 error

---
+++ Front/Cooperadora-Escuela/src/app/pages/404-error/404-error.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-404-error',
+  templateUrl: './404-error.component.html',
+  styleUrls: ['./404-error.component.css']
+})
+export class 404ErrorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
